9fbc549fc7b215089c0c37313852627026caabfa
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-05 14:57:11 +0530

    add jumbotron component as homepage

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Inner, Item, Container } from './styles/jumbotron'
+
+export default function Jumbotron({ children, direction, ...restProps }) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  )
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>
+}
