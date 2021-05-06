fa48ab62d0d6fc4be972194a26d28d3d11c5aad9
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-06 17:30:07 +0530

    add faqs section

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Body = styled.body``
+
+export const Container = styled.section``
+
+export const Header = styled.div``
+
+export const Inner = styled.div``
+
+export const Item = styled.div``
+
+export const Title = styled.h1``
