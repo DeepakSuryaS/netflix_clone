2ce4b57bdd25c655d9ed96f68066e49415d9212d
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-07 17:51:51 +0530

    add form and setup sign in page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles'
 import { App } from './app';
+import { FirebaseContext } from './context/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeI_Xne0l6QjylBw5CCNvuEJi_sdyt4S4",
+  authDomain: "netflix-clone-dc8a6.firebaseapp.com",
+  projectId: "netflix-clone-dc8a6",
+  storageBucket: "netflix-clone-dc8a6.appspot.com",
+  messagingSenderId: "779102495203",
+  appId: "1:779102495203:web:7e9c5733c0563bcc4380d6",
+  //databaseURL: 'https://netflix-c8ae9.firebaseio.com'
+}
+
+//const firebase = window.firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>, 
   document.getElementById('root')
 );
