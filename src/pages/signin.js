4b63c74d8621768200479e50f297c9a9c97569c4
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-07 18:31:22 +0530

    add basic validation for signin form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -15,6 +15,22 @@ export default function Signin() {
     // populate error if any
   }
 
+  const validateField = (value, field) => {
+    const errorMsg = 'Please check your email and/or password'
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    // at least one lowercase, one uppercase, one number and six characters long
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    let valid = true;
+
+    if (field === 'email') {
+      valid = emailRegex.test(value);
+    } else {
+      valid = passwordRegex.test(value);
+    }
+
+    valid ? setError('') : setError(errorMsg);
+  }
+
   return (
     <HeaderContainer>
       <Form>
@@ -25,14 +41,20 @@ export default function Signin() {
           <Form.Input 
             placeholder="Email Address"
             value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
+            onChange={({ target }) => {
+              setEmailAddress(target.value)
+              validateField(target.value, 'email')
+            }}
           />
           <Form.Input 
             placeholder="Password"
             type="password"
             value={password}
             autoComplete="off"
-            onChange={({ target }) => setPassword(target.value)}
+            onChange={({ target }) => {
+              setPassword(target.value)
+              validateField(target.value, 'password')
+            }}
           />
           <Form.Submit disabled={false} type="submit">
             Sign In
