f3f2719ad260a902dc95895c31b3087aa3936e6b
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-08 22:09:13 +0530

    add header to browse component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link as ReachRouterLink } from 'react-router-dom'
-import { Background, Container, Logo, ButtonLink } from './styles/header'
+import { Background, Container, Logo, ButtonLink, Link, Text, Group } from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -8,6 +8,18 @@ export default function Header({ bg = true, children, ...restProps }) {
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>
 }
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
-import { Header } from '../components/header'
+import { Header } from '../components'
 import * as ROUTES from '../constants/routes'
 import { FirebaseContext } from '../context/firebase'
 import { SelectProfileContainer } from './profiles'
 import { FooterContainer } from './footer'
 
 export function BrowseContainer() {
+  const [category, setCategory] = useState('series')
   const [profile, setProfile] = useState({})
+  const [loading, setLoading] = useState(false)
 
   const user = {
     displayName: "Deepak Surya",
@@ -15,7 +17,25 @@ export function BrowseContainer() {
 
   return profile.displayName ? (
     <>
-      <p>Browse Container</p>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix" />
+            <Header.Link
+              active={category === 'series'}
+              onClick={() => setCategory('series')}
+            >
+              Series
+            </Header.Link>
+            <Header.Link
+              active={category === 'films'}
+              onClick={() => setCategory('films')}
+            >
+              Films
+            </Header.Link>
+          </Header.Group>
+        </Header.Frame>
+      </Header>
       <FooterContainer />
     </>
   ) : <SelectProfileContainer user={user} setProfile={setProfile} />
