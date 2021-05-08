fa5e00c3225a20fde7def141da1e12d5fcdccdb7
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-08 19:27:48 +0530

    style profile section

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -1,13 +1,55 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  max-width: 80%;
+`
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+  width: 100%;
+  font-size: 48px;
+  font-weight: 500;
+  color: #fff;
+  text-align: center;
+`
 
-export const List = styled.ul``
+export const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+`
 
-export const Name = styled.p``
+export const Name = styled.p`
+  color: #808080;
+  text-overflow: ellipsis;
+  font-size: 16px;
+`
 
-export const Picture = styled.img``
+export const Picture = styled.img`
+  width: 100%;
+  max-width: 150px;
+  height: auto;
+  border: 3px solid black;
+  cursor: pointer;
+`
 
-export const Item = styled.li``
+export const Item = styled.li`
+  max-height: 200px;
+  max-width: 200px;
+  list-style-type: none;
+  text-align: center;
+
+  &:hover > ${Picture} {
+    border: 3px solid white;
+  }
+
+  &:hover ${Name} {
+    font-weight: bold;
+    color: white;
+  }
+`
