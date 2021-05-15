0ec492261ceefe6b35cfdea7bc0a4b0c93275cf3
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-15 19:59:08 +0530

    group data to categories of films, series

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowseContainer } from '../containers/browse'
 import { useContent } from '../hooks'
+import { selectionMap } from '../utils'
 
 export default function Browse() {
   const { series } = useContent('series')
   const { films } = useContent('films')
-
+  const slides = selectionMap({ series, films })
+  
   return (
-    <BrowseContainer />
+    <BrowseContainer slides={slides} />
   )
 }
