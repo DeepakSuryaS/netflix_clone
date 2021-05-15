037650343bbc27653aa3781a6c4d580887541152
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-15 19:36:40 +0530

    setup custom hook to get firebase data

---
+++ src/hooks/use-content.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { FirebaseContext } from '../context/firebase'
+
+export default function useContent(target) {
+  const [content, setContent] = useState([])
+  const { firebase } = useContext(FirebaseContext)
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => {
+        const allContent = snapshot.docs.map((obj) => ({
+          ...obj.data(),
+          docId: obj.id
+        }))
+
+        setContent(allContent)
+      })
+      .catch(error => console.log(error.message))
+  }, [])
+
+  return { [target]: content }
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { BrowseContainer } from '../containers/browse'
+import { useContent } from '../hooks'
 
 export default function Browse() {
+  const { series } = useContent('series')
+  const { films } = useContent('films')
+
   return (
     <BrowseContainer />
   )
