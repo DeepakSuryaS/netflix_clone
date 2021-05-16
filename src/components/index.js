32183c162b15251d6e8b12464970fe0c4dfb771b
Deepak Surya S <ideepaksuryas@gmail.com>
2021-05-16 19:32:08 +0530

    add card component and styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Accordion } from './accordion'
+export { default as Card } from './card'
 export { default as Footer } from './footer'
 export { default as Feature } from './feature'
 export { default as Form } from './form'
